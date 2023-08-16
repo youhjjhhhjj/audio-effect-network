cb1d314a0c7bdd55838242d7aa62d5ba771d7fff
youhjjhhhjj <allenli.comix@gmail.com>
2023-08-15 22:02:05 -0400

    use argparse command line arguments

--- effect.py
+++ effect.py
@@ -5,35 +5,37 @@ import torchaudio
 
 import matplotlib.pyplot as plt
 import sys
+import argparse
 
 from random import randint
 
-def load_wav(path):
-    data_waveform, sample_rate = torchaudio.load(path, format="wav")
-    assert sample_rate == _sample_rate  # rate matches
-    assert data_waveform.size()[0] == 1  # mono
-    return downsampler(data_waveform)
+def load_wav(path, sample_rate):
+    # TODO resample if necessary
+    wav = torchaudio.load(path, format="wav")
+    assert wav[1] == sample_rate  # rate matches
+    assert wav[0].size()[0] == 1  # mono
+    return downsampler(wav[0])
 
-def train(model, train_data, test_data, epochs, learning_rate=0.001):
-    data_length = min(train_data.size()[1], test_data.size()[1])
-    train_data = train_data[:, : data_length]
-    test_data = test_data[:, : data_length]
+def train(model, input_data, target_data, epochs, learning_rate=0.001):
+    data_length = min(input_data.size()[1], target_data.size()[1])
+    input_data = input_data[:, : data_length]
+    target_data = target_data[:, : data_length]
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
     losses = []
     for i in range(epochs):
         # create training sequence
         index = randint(0, data_length - model_width)
-        train_seq = train_data[:, index : index + model_width]
-        test_seq = test_data[:, index : index + model_width]
-        scale, shift = get_data_scale(train_seq)
-        train_seq = train_seq * scale + shift
-        test_seq = test_seq * scale + shift
+        input_seq = input_data[:, index : index + model_width]
+        target_seq = target_data[:, index : index + model_width]
+        scale, shift = get_data_scale(input_seq)
+        input_seq = input_seq * scale + shift
+        target_seq = target_seq * scale + shift
         # run backward pass
         optimizer.zero_grad()
-        outputs = model(train_seq)
+        outputs = model(input_seq)
         # loss_function = nn.MSELoss()
-        loss = (test_seq - outputs).square().sum()
-        # loss = loss_function(outputs, test_seq)
+        loss = (target_seq - outputs).square().sum()
+        # loss = loss_function(outputs, target_seq)
         print(loss)
         loss.backward()
         optimizer.step()
@@ -60,30 +62,37 @@ def get_data_scale(data):
     shift = -1 - (data_min * scale)
     return scale, shift
 
-if __name__ == "__main__":    
-    epochs = int(sys.argv[3])
-    hidden_ratio = float(sys.argv[4])
-    network_dim_s = float(sys.argv[5])  # number of seconds
-    downsample_ratio = int(sys.argv[6])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files",nargs=2)
+    parser.add_argument("-epochs", help="Number of training epochs", type=int, default=5)
+    parser.add_argument("-hidden_ratio", "-hr", help="Ratio of hidden dimension to input dimension", type=float, default=1.0)
+    parser.add_argument("-network_dim", "-nd", help="How many seconds of audio for the network to process at once", type=float, default=1)
+    parser.add_argument("-downsample_ratio", "-dr", help="Factor by which to downsample inputs for faster running", type=float, default=4)
+    parser.add_argument("-sample_rate", "-sr", "-hz", help="Frequency of input audio", type=int, default=44100)
 
-    _sample_rate = 44100
+    args=parser.parse_args()
+    epochs = args.epochs
+    hidden_ratio = args.hidden_ratio
+    network_dim = args.network_dim
+    downsample_ratio = args.downsample_ratio
+    sample_rate = args.sample_rate
 
-    downsample_rate = _sample_rate // downsample_ratio
-    model_width = int(network_dim_s * downsample_rate)
+    downsample_rate = int(sample_rate / downsample_ratio)
+    model_width = int(network_dim * downsample_rate)
 
     downsampler = torchaudio.transforms.Resample(
-        _sample_rate, downsample_rate, resampling_method='sinc_interpolation')
+        sample_rate, downsample_rate, resampling_method='sinc_interpolation')
 
-    train_data = load_wav(sys.argv[1])
-    test_data = load_wav(sys.argv[2])    
+    input_data = load_wav(sys.argv[1], sample_rate)
+    target_data = load_wav(sys.argv[2], sample_rate)    
     
     net = Feedforward(model_width, hidden_ratio)
     print(net)
-    losses = train(net, train_data, test_data, epochs)
-    # losses = train(net, train_data[:, :model_width], test_data[:, :model_width], 10)
+    losses = train(net, input_data, target_data, epochs)
     plt.figure()
     plt.plot(losses)
     plt.savefig("losses.png")
 
-    prediction = predict(net, train_data)
+    prediction = predict(net, target_data)
     torchaudio.save('prediction.wav', prediction, downsample_rate)
--- models/feedforward.py
+++ models/feedforward.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class Feedforward(nn.Module):
     def __init__(self, model_width, hidden_ratio):
         super(Feedforward, self).__init__()
-        hidden_dim = int(model_width // hidden_ratio)
+        hidden_dim = int(model_width * hidden_ratio)
         self.fc1 = nn.Linear(model_width, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
         self.fc3 = nn.Linear(hidden_dim, hidden_dim)
