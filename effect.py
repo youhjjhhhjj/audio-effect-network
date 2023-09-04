e19f982ee036602f8a89b4ffc51ae611e7c75af0
youhjjhhhjj <allenli.comix@gmail.com>
2023-09-04 18:04:32 -0400

    enable batching of inputs

--- effect.py
+++ effect.py
@@ -1,10 +1,10 @@
 from models import Feedforward
 import torch
-import torch.nn.functional as F
 import torchaudio
 
 import matplotlib.pyplot as plt
 import sys
+import os
 import argparse
 from pathlib import Path
 
@@ -13,27 +13,34 @@ from random import randrange
 
 def load_wav(path, sample_rate):
     # TODO resample if necessary
-    wav = torchaudio.load(path, format="wav")
-    assert wav[1] == sample_rate  # rate matches
-    if wav[0].shape[0] != 1:
-        print("Audio file is stereo, proceeding as mono: " + path)
-    return downsampler(wav[0][0])
+    waveform, sr = torchaudio.load(path, format="wav")
+    assert sr == sample_rate  # rate matches
+    if waveform.shape[0] != 1:
+        print("Audio file is stereo, proceeding as mono: " + str(path))
+        waveform = torch.mean(waveform, dim=0, keepdim=True)
+    return downsampler(waveform[0])
 
-cossim = torch.nn.CosineSimilarity(dim=0)
+cossim = torch.nn.CosineSimilarity(dim=1)
 
-def train(model, input_data, target_data, epochs, learning_rate=0.0001):
-    data_length = min(input_data.shape[1], target_data.shape[1])
+def train(model, input_dataset, target_dataset, epochs, batch_size=8, learning_rate=0.0001):
+    dataset_length = len(input_dataset)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     losses = []
     for i in range(epochs):
-        # create training sequence
-        index = randrange(0, data_length)
-        input_seq = input_data[:, index]
-        target_seq = target_data[:, index]
+        input_batch = []
+        target_batch = []
+        for j in range(batch_size):
+            input_data = input_dataset[j % dataset_length]
+            target_data = target_dataset[j % dataset_length]
+            # create training sequence
+            data_length = min(input_data.shape[1], target_data.shape[1])
+            index = randrange(0, data_length)
+            input_batch.append(input_data[:, index])
+            target_batch.append(target_data[:, index])
         # run backward pass
         optimizer.zero_grad()
-        outputs = model(input_seq)
-        loss = 1 - cossim(outputs, target_seq).sum()
+        outputs = model(torch.stack(input_batch, dim=0))
+        loss = 1 - cossim(outputs, torch.stack(target_batch, dim=0)).mean()
         print(loss)
         loss.backward()
         optimizer.step()
@@ -61,6 +68,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("files",nargs=2)
     parser.add_argument("-epochs", help="Number of training epochs", type=int, default=5)
+    parser.add_argument("-batch_size", "-bs", help="Size of training batch", type=int, default=8)
     parser.add_argument("-hidden_ratio", "-hr", help="Ratio of hidden dimension to input dimension", type=float, default=1.0)
     parser.add_argument("-n_fft", help="Number of fft bins for spectrogram", type=int, default=512)
     parser.add_argument("-downsample_ratio", "-dr", help="Factor by which to downsample inputs for faster running", type=float, default=4)
@@ -69,34 +77,39 @@ if __name__ == "__main__":
 
     args=parser.parse_args()
     epochs = args.epochs
+    batch_size = args.batch_size
     hidden_ratio = args.hidden_ratio
     n_fft = args.n_fft
     downsample_ratio = args.downsample_ratio
     sample_rate = args.sample_rate
     save_model_name = args.save_model
+    input_dir = Path(sys.argv[1]).resolve()
+    target_dir = Path(sys.argv[2]).resolve()
 
     downsample_rate = int(sample_rate / downsample_ratio)
     model_dim = n_fft // 2 + 1
     
     downsampler = torchaudio.transforms.Resample(sample_rate, downsample_rate, resampling_method='sinc_interpolation')    
 
-    input_audio = load_wav(sys.argv[1], sample_rate)
-    target_audio = load_wav(sys.argv[2], sample_rate)    
+    input_dataset = []
+    target_dataset = []
 
-    input_spectrum = torch.stft(input_audio, n_fft, return_complex=True)
-    input_data = input_spectrum.abs().squeeze(dim=0)
-    target_data = torch.stft(target_audio, n_fft, return_complex=True).abs().squeeze(dim=0)
+    for input_file, target_file in zip(sorted(os.listdir(sys.argv[1])), sorted(os.listdir(sys.argv[2]))):
+        input_waveform = load_wav(input_dir/input_file, sample_rate)
+        target_waveform = load_wav(target_dir/target_file, sample_rate)
+        input_dataset.append(torch.stft(input_waveform, n_fft, return_complex=True).abs())
+        target_dataset.append(torch.stft(target_waveform, n_fft, return_complex=True).abs())
     
     net = Feedforward(model_dim, hidden_ratio)
     print(net)
     print(sum(p.numel() for p in net.parameters()))
-    losses = train(net, input_data, target_data, epochs)
+    losses = train(net, input_dataset, target_dataset, epochs, batch_size)
     if save_model_name != "":
         torch.save(net.state_dict(), Path().absolute() / "weights" / (save_model_name + ".pth"))
     plt.figure()
     plt.plot(losses)
     plt.savefig("losses.png")
 
-    prediction = predict(net, input_data)
-    prediction_audio = torch.istft(torch.polar(prediction, input_spectrum.angle().squeeze(dim=0)), n_fft, return_complex=False)
-    torchaudio.save('prediction.wav', prediction_audio.unsqueeze(0), downsample_rate)
+    # prediction = predict(net, input_data)
+    # prediction_audio = torch.istft(torch.polar(prediction, input_spectrum.angle().squeeze(dim=0)), n_fft, return_complex=False)
+    # torchaudio.save('prediction.wav', prediction_audio.unsqueeze(0), downsample_rate)
